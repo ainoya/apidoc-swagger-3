d0d0f9c496b5d255b364a629bcc36ba0e5b77194
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-03-10 16:31:38 +0100

    Moving from apidoc-core lib to apidoc lib

--- lib.js
+++ lib.js
@@ -1,13 +1,10 @@
 const fs = require('fs')
 const path = require('path')
-const apidoc = require('apidoc-core')
+const apidoc = require('apidoc')
 const winston = require('winston');
 const yaml = require('js-yaml')
 
 const apidoc_to_swagger = require('./apidoc_to_swagger');
-const libPkg = require('./package.json');
-
-apidoc.setGeneratorInfos({ name: libPkg.name, time: new Date(), version: libPkg.version, url: libPkg.repository.url })
 
 
 function generateLog(options) {
@@ -27,10 +24,8 @@ function generateLog(options) {
 function main(options) {
     options.verbose && console.log('options', options);
     const log = generateLog(options)
-    const { src, dest, verbose } = options
-    apidoc.setLogger(log)
 
-    var api = apidoc.parse({ ...options, log: log })
+    var api = apidoc.createDoc({ ...options, log: log })
     if (api === true) {
         console.log('No input data found, check your include/exclude filters');
         return;
@@ -42,25 +37,22 @@ function main(options) {
     const swagger = apidoc_to_swagger.toSwagger(apidocData, projectData)
 
     api["swaggerData"] = swagger;
-    createOutputFile(api.swaggerData, log, options)
+    createOutputFiles(api.swaggerData, log, options)
 
     return swagger;
 }
 
-function createOutputFile(swaggerData, log, options) {
-    if (options.simulate)
-        log.warn('!!! Simulation !!! No file or dir will be copied or created.');
-
+function createOutputFiles(swaggerData, log, options) {
     log.verbose('Creating dir: ' + options.dest);
-    if (!options.simulate)
+    if (!options.dryRun)
         fs.existsSync(options.dest) || fs.mkdirSync(options.dest);
 
     log.verbose('Writing JSON swagger file: ' + options.dest + 'swagger.json');
-    if (!options.simulate)
+    if (!options.dryRun)
         fs.writeFileSync(options.dest + './swagger.json', JSON.stringify(swaggerData, null, 4));
 
     log.verbose('Writing YAML swagger file: ' + options.dest + 'swagger.yaml');
-    if (!options.simulate)
+    if (!options.dryRun)
         fs.writeFileSync(options.dest + './swagger.yaml', yaml.dump(swaggerData));
 }
 
